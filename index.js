26f797df6cce2514be851af3410b271449e4be89
Eli Morris-Heft <eli.morris.heft@gmail.com>
2022-01-18 22:37:52 -0500

    Switch minimist for commander

--- index.js
+++ index.js
@@ -2,18 +2,16 @@ import path from 'path';
 import fs from 'fs/promises';
 
 import log from 'loglevel';
-import parseArgs from 'minimist';
 import commander from 'commander';
 import chalk from 'chalk';
 
 import makeDir from 'make-dir';
 
-import { LiveContainer } from 'clui-live';
+import { LiveArea, LiveContainer } from 'clui-live';
 import timeSpan from 'time-span';
 import prettyMilliseconds from 'pretty-ms';
 
 import configurePuzzleLogging from './lib/log.js';
-import exp from 'constants';
 
 const __filename = import.meta.url.replace('file://', '');
 const __dirname = path.dirname(__filename);
@@ -25,168 +23,240 @@ main().catch(
 async function main() {
   const program = new commander.Command();
 
+  const VALID_INPUT_TYPES = ['all', 'real', 'tests'];
+  const VALID_LOG_LEVELS = [ 'trace', 'debug', 'info', 'warn', 'error', 'silent' ];
   program
     .command('run', { isDefault: true })
     .description('run a puzzle')
-    .option('-i, --inputs <type>', 'designate which input types to use (`all`, `real`, `tests`)', 'real')
-    .option('--log-level <level>', 'set the logging level (`trace`, `debug`, `info`, `warn`, `error`, `silent`)', 'warn')
-    .option('-d, --debug', 'alias for --log-level=debug')
-    .option('-s, --silent', 'alias for --log-level=silent')
-    .argument('[year]', 'the year of the puzzle to run', 'latest')
-    .argument('[day]', 'the day of the puzzle to run', 'latest')
-    .argument('[part]', 'the part of the puzzle to run', 'all')
-    .action((year, day, part, options, command) => {
+    .option('-i, --inputs <type>', 'designate which input types to use (`all`, `real`, `tests`)', createEnumValidator(VALID_INPUT_TYPES), 'real')
+    .option('--all', 'alias for --inputs=all')
+    .option('--tests', 'alias for --inputs=tests')
+    .option('-f, --file <file>', 'use the given file (relative to puzzle directory) as input (overrides --inputs)')
+    .option('--log-level <level>', 'set the logging level (`trace`, `debug`, `info`, `warn`, `error`, `silent`)', createEnumValidator(VALID_LOG_LEVELS), 'warn')
+    .option('-d, --debug', 'alias for --log-level=debug (overrides --log-level)')
+    .option('-s, --silent', 'alias for --log-level=silent (overrides --log-level')
+    .argument('[year]', 'the year of the puzzle to run', createTestValidator(value => value === 'latest' || validateYear(value), 'a four-digit number'), 'latest')
+    .argument('[day]', 'the day of the puzzle to run', createTestValidator(value => value === 'latest' || validateDay(value), 'a one- or two-digit number'), 'latest')
+    .argument('[part]', 'the part of the puzzle to run', createTestValidator(validatePart, 'a one-digit number'), 1)
+    .action(async (year, day, part, options, command) => {
+      let logLevel = options.logLevel;
+      if(options.debug) {
+        logLevel = 'debug';
+      } else if(options.silent) {
+        logLevel = 'silent';
+      }
+      configurePuzzleLogging(logLevel);
 
+      // figure out inputs
+      const inputs = {
+        file: null,
+        real: false,
+        tests: false,
+      }
+      if(['real', 'all'].includes(options.inputs) || options.all) {
+        inputs.real = true;
+      }
+      if(['tests', 'all'].includes(options.inputs) || options.all || options.tests) {
+        inputs.tests = true;
+      }
+      if(options.file) {
+        inputs.file = options.file;
+      }
+
+      // fill in year/day/part
+      if(year === 'latest') {
+        year = await getLatestYear();
+      } else {
+        year = +year;
+      }
+
+      if(day === 'latest') {
+        day = await getLatestDay(year);
+      } else {
+        day = +day;
+      }
+
+      part = +part;
+
+      // run the puzzle
+      run(year, day, part, { inputs });
     });
 
   program
     .command('new')
     .description('set up files for a new puzzle')
-    .argument('[year]', 'which year the new puzzle is in', 'next')
-    .argument('[day]', 'which day the new puzzle is in', 'next')
-    .action((year, day, options, command) => {
+    .argument('[year]', 'which year the new puzzle is in', createTestValidator(value => value === 'next' || validateYear(value), 'a four-digit number'), 'next')
+    .argument('[day]', 'which day the new puzzle is for', createTestValidator(value => value === 'next' || validateDay(value), 'a one- or two-digit number'), 'next')
+    .action(async (year, day, options, command) => {
+      console.debug({year, day, options});
+      // fill in year/day/part
+      if(year === 'next' && day === 'next') {
+        year = await getLatestYear();
+        day = await getLatestDay(year);
 
+        day++;
+        if(day > 25) {
+          year++;
+          day = 1;
+        }
+      } else if(day === 'next') {
+        year = +year;
+        day = await getLatestDay(year);
+
+        day++;
+        if(day > 25) {
+          console.warn(`All the days for ${year} are accounted for. Cowardly refusing to create ${year + 1} Day 1 when ${year} was explicitly specified.`);
+          process.exitCode = 1;
+          return;
+        }
+      } else if(year === 'next') {
+        console.warn(`You want me to create a specific day but in whatever the next year is? That's probably not actually what you want me to do. Please specify the year.`);
+        process.exitCode = 1;
+        return;
+      }
+
+      console.debug({year, day});
+      createPuzzle(year, day);
     });
 
-  const argv = parseArgs(process.argv.slice(2), {
-    string: ['inputs', 'file'],
-    boolean: [
-      'help', 'new',
-      'silent', 'debug',
-      'timer',
-    ],
-    default: {
-      inputs: 'real',
-      timer: true,
-      logs: 'warn',
-    },
-  });
-
-  // Display help text?
-  if(argv.help) {
-    help();
-  }
-
-  // Configure the log for logging inside puzzle solutions
-  if(argv.silent) {
-    argv.logs = 'silent';
-  } else if(argv.debug) {
-    argv.logs = 'debug';
-  }
-
-  const VALID_LOG_LEVELS = [ 'trace', 'debug', 'info', 'warn', 'error', 'silent' ];
-  if(!VALID_LOG_LEVELS.includes(argv.logs)) {
-    process.exitCode = 1;
-    throw new Error(`Invalid log level '${argv.logs}' given. Options are: ${VALID_LOG_LEVELS.join(',')}`);
-  }
-  configurePuzzleLogging(argv.logs);
-
-
-  // Validate puzzle address inputs
-  const [ year, day, part ] = argv._;
-  validatePuzzleAddress(year, day, part, !argv.new);
-
-  // Are we creating a new puzzle scaffold?
-  if(argv.new) {
-    await create(year, day);
-    return;
-  }
-
-  // Set the run options
-  const options = {
-    timer: true,
-    input: {
-      real: false,
-      tests: false,
-      file: null,
-    },
-  };
-
-  if(['real', 'all'].includes(argv.inputs)) {
-    options.input.real = true;
-  }
-
-  if(['tests', 'all'].includes(argv.inputs)) {
-    options.input.tests = true;
-  }
-
-  if(argv.file) {
-    options.input.file = argv.file;
-  }
-
-  // passing --no-timer will set timer to false
-  options.timer = argv.timer;
-
-  run(year, day, part, options);
+  program.parse(process.argv);
 }
 
-async function run(year, day, part, options = { timer: true, input: { file: null, real: true, tests: false }}) {
-  const logContainer = new LiveContainer().hook();
+function createEnumValidator(validValues) {
+  return function(value) {
+    if(!validValues.includes(value)) {
+      throw new commander.InvalidArgumentError(`Valid values are: ${validValues.join(', ')}.`);
+    }
+
+    return value;
+  };
+}
+
+function createTestValidator(testFn, optionName, mustBeDescription) {
+  return function(value) {
+    if(!testFn(value)) {
+      throw new commander.InvalidArgumentError(`Must be ${mustBeDescription}.`);
+    }
+
+    return value;
+  }
+}
+
+async function getLatestYear() {
+  const entries = await fs.readdir(path.join(__dirname, 'puzzles'));
+  const EXCLUDE = [ '2021' ]; // exclude 2021 because it's already done; will disable next year
+  const years = entries.filter(name => validateYear(name)).filter(name => !EXCLUDE.includes(name)).map(x => +x);
+  return Math.max(...years);
+}
+
+async function getLatestDay(year) {
+  const entries = await fs.readdir(path.join(__dirname, 'puzzles', year.toString(10)));
+  const days = entries.filter(name => validateDay(name)).map(x => +x);
+  return Math.max(...days);
+}
+
+function validateYear(year) {
+  return /^\d{4}$/.test(year.toString(10));
+}
+
+function validateDay(day) {
+  return /^\d{1,2}$/.test(day.toString(10));
+}
+
+function validatePart(part) {
+  return /^\d$/.test(part.toString(10));
+}
+
+async function run(year, day, part, options = { inputs: { file: null, real: true, tests: false } }) {
+  // gather everything we'll need
+  const PUZZLE_DIR = path.join(__dirname, `puzzles/${year}/${day}`);
+  const fn = await getPuzzleFn(PUZZLE_DIR, part);
+  const attempts = await gatherAttempts(PUZZLE_DIR, part, options.inputs);
 
   console.log(`Running puzzle 📯 ${year} 🌅 ${day} 🧩 ${part}:`);
 
-  const puzzleDir = path.join(__dirname, `puzzles/${year}/${day}`);
+  // first, run the tests
+  for(let attempt of attempts.tests) {
+    const updateState = makeUpdateState(attempt.name, true, attempt.expected);
+    await runAttempt(fn, attempt, updateState);
+  }
 
-  // Import the puzzle function
+  // then run the real thing
+  if(attempts.real) {
+    const updateState = makeUpdateState(attempts.real.name);
+    await runAttempt(fn, attempts.real, updateState);
+  }
+}
+
+async function getPuzzleFn(puzzleDir, part) {
   const parts = await import(path.join(puzzleDir, 'puzzle.js'));
+
   const fn = parts.default['part' + part];
   if(typeof fn !== 'function') {
     throw new Error(`Puzzle ${year}/${day} part ${part} is not a function (which probably means it doesn't exist)!`);
   }
 
-  // Gather the input files and expected outputs
-  const files = { real: null, tests: [] };
+  return fn;
+}
 
-  if(options.input.real || options.input.file) {
-    const realFilename = options.input.file || 'input.txt';
-    files.real = await fs.readFile(path.join(puzzleDir, realFilename), { encoding: 'utf-8' });
+async function gatherAttempts(puzzleDir, part, inputTypes = { file: null, real: true, tests: false }) {
+  const attempts = { real: null, tests: [] };
+
+  if(inputTypes.real || inputTypes.file) {
+    const filename = inputTypes.file || 'input.txt';
+    const contents = await fs.readFile(path.join(puzzleDir, filename), { encoding: 'utf-8' });
+
+    attempts.real = {
+      name: filename,
+      input: contents,
+      expected: null,
+      options: null,
+    };
   }
 
-  if(options.input.tests) {
-    const expectedContents = await fs.readFile(path.join(puzzleDir, 'expected.json'), { encoding: 'utf-8' });
-    let expected = JSON.parse(expectedContents).filter(obj => obj.part === part);
+  if(inputTypes.tests) {
+    const expected = (await gatherExpectedValues(puzzleDir)).filter(obj => obj.part === part);
 
     for(let entry of expected) {
       const contents = await fs.readFile(path.join(puzzleDir, entry.file), { encoding: 'utf-8' });
-      files.tests.push({
+      attempts.tests.push({
         name: entry.file,
-        contents,
+        input: contents,
         expected: entry.output === undefined ? null : entry.output,
         options: entry.options === undefined ? null : entry.options,
       });
     }
   }
 
-  // Run each example input
-  for(let example of files.tests) {
-    const updateArea = logContainer.createLiveArea().pin();
-    const updateState = makeUpdateState(example.name, true, example.expected);
-    drawStatusArea(updateArea, updateState);
+  return attempts;
+}
 
-    const { result, elapsed } = await runFn(fn, example.contents, example.options);
+async function gatherExpectedValues(puzzleDir) {
+  let expected = [];
 
-    updateState.result = result;
-    updateState.elapsed = elapsed;
-    if(example.expected !== null) {
-      updateState.isPass = result === example.expected;
-    }
-    drawStatusArea(updateArea, updateState);
-    updateArea.close();
+  try {
+    const expectedContents = await fs.readFile(path.join(puzzleDir, 'expected.json'), { encoding: 'utf-8' });
+    expected = JSON.parse(expectedContents);
+  } catch(e) {
+    console.warn(`Could not load or parse expected.json (${e.message}). Continuing without it...`);
   }
 
-  // Run the real input
-  if(files.real !== null) {
-    const updateArea = logContainer.createLiveArea().pin();
-    const updateState = makeUpdateState('input.txt', false);
-    drawStatusArea(updateArea, updateState);
+  return expected;
+}
 
-    const { result, elapsed } = await runFn(fn, files.real);
+async function runAttempt(fn, attempt, updateState) {
+  const updateArea = new LiveArea().hook().pin();
+  drawStatusArea(updateArea, updateState);
 
-    updateState.result = result;
-    updateState.elapsed = elapsed;
-    drawStatusArea(updateArea, updateState);
-    updateArea.close();
+  const { result, elapsed } = await runFn(fn, attempt.input, attempt.options);
+
+  updateState.result = result;
+  updateState.elapsed = elapsed;
+  if(attempt.expected !== null) {
+    updateState.isPass = result === attempt.expected;
   }
+  drawStatusArea(updateArea, updateState);
+  updateArea.close();
 }
 
 async function runFn(fn, input, options = null) {
@@ -202,7 +272,7 @@ async function runFn(fn, input, options = null) {
   };
 }
 
-function makeUpdateState(name, isTest, expected = null) {
+function makeUpdateState(name, isTest = false, expected = null) {
   return {
     name,
     isTest,
@@ -246,84 +316,43 @@ function drawStatusArea(area, updateState) {
   area.write(chalk[color](`${icon} ${updateState.name}: ${message}${time}`));
 }
 
-async function create(year, day) {
-  log.info(`Creating a puzzle scaffold for ${year}/${day}...`);
+async function createPuzzle(year, day) {
+  const SKELETON_DIR = path.join(__dirname, 'skel');
+  const PUZZLE_DIR = path.join(__dirname, 'puzzles', year.toString(), day.toString());
 
-  // Determine if the desired puzzle does in fact exist
-  const puzzleDir = path.join(__dirname, `puzzles/${year}/${day}`);
-  log.debug(`Checking if ${puzzleDir} exists...`);
+  const container = new LiveContainer().hook();
+
+  // first, determine if the desired puzzle already exists
+  const checkArea = container.createLiveArea();
+  checkArea.write(`Checking if the puzzle directory for ${year}/${day} (${PUZZLE_DIR}) exists...`);
   try {
-    await fs.access(puzzleDir);
-
-    // if we're here, the directory exists, so warn and bail
-    log.warn(`The puzzle directory for ${year}/${day} (${puzzleDir}) already exists! Nothing left to do.`);
+    await fs.access(PUZZLE_DIR);
+    // if we're still here, the directory exists, so warn and bail
+    checkArea.write(`Checking if the puzzle directory for ${year}/${day} (${PUZZLE_DIR}) exists... it does! Nothing left to do.`);
+    checkArea.close();
     return;
-  } catch(e) {
-    // keep going to create the directory
+  } catch(x) {
+    // the directory does not exist, so keep going
+    checkArea.write(`Checking if the puzzle directory for ${year}/${day} (${PUZZLE_DIR}) exists... nope. Good!`);
+    checkArea.close();
   }
 
-  // Create the new puzzle directory
-  log.debug(`Creating ${puzzleDir}...`);
-  await makeDir(puzzleDir);
+  const createArea = container.createLiveArea();
+  createArea.write(`Creating ${PUZZLE_DIR}...`);
+  await makeDir(PUZZLE_DIR);
+  createArea.write(`Creating ${PUZZLE_DIR}... done!`);
+  createArea.close();
 
-  const skelDir = path.join(__dirname, 'skel');
-  log.debug(`Cloning the skeleton directory from ${skelDir} into ${puzzleDir}...`);
-  const skelFiles = await fs.opendir(skelDir);
+  const cloneArea = container.createLiveArea();
+  cloneArea.write(`Cloning the puzzle skeleton into ${PUZZLE_DIR}...`);
+  const skelFiles = await fs.opendir(SKELETON_DIR);
   for await(let file of skelFiles) {
     if(file.isFile()) {
-      log.debug(`  - copying ${file.name}`);
-      await fs.copyFile(path.join(skelDir, file.name), path.join(puzzleDir, file.name));
+      console.log(`  - ${file.name}`);
+      await fs.copyFile(path.join(SKELETON_DIR, file.name), path.join(PUZZLE_DIR, file.name));
     }
   }
-  log.debug('Cloning complete.');
-}
 
-function validatePuzzleAddress(year, day, part = null, partRequired = false) {
-  if(!/\d{4}/.test(year.toString(10))) {
-    throw new Error(`YEAR must be a 4-digit number (YEAR was: ${year})`);
-  }
-
-  if(!/\d{1,2}/.test(day.toString(10))) {
-    throw new Error(`DAY must be a 1- or 2-digit number (DAY was: ${day})`);
-  }
-
-  if(partRequired) {
-    if(part === undefined || part === null) {
-      throw new Error(`PART is required when running a puzzle. (PART was: ${part})`);
-    }
-
-    if(!/\d{1}/.test(part.toString(10))) {
-      throw new Error(`PART must be a 1-digit number (PART was: ${part})`);
-    }
-  }
-}
-
-function help(exitCode = 0) {
-  const HELP_TEXT = `
-${chalk.bold('Advent of Code Puzzle Runner')}
-
-  To run a puzzle:
-  ${chalk.bold('node ./index.js [options] <YEAR> <DAY> <PART>')}
-
-  To create a new puzzle template:
-  ${chalk.bold('node ./index.js --new <YEAR> <DAY>')}
-
-  To view this help:
-  ${chalk.bold('node ./index.js --help')}
-
-${chalk.bold('OPTIONS:')}
-  --inputs=<TYPE>   Run the puzzle with the given input types. Options are: ${chalk.yellow('real')}, tests, all.
-                    Defaults to '${chalk.yellow('real')}'.
-  --file=<FILE>     Run the puzzle using <FILE> as the input. <FILE> is relative to the puzzle directory, so it should
-                    probably look like "test.txt" or "input.txt", no other path needed.
-  --timer           Display the time the puzzle took to run for each input. ${chalk.yellow('On by default.')}
-  --no-timer        Do not display the time the puzzle took to run for each input.
-  --logs=<LEVEL>    Set the logging level inside puzzles to <LEVEL>. Options are: trace, debug, info, ${chalk.yellow('warn')}, error,
-                    silent. Defaults to '${chalk.yellow('warn')}'.
-  --debug           Alias for ${chalk.white('--logs=debug')}.
-  --silent          Alias for ${chalk.white('--logs=silent')}.
-  `;
-
-  console.log(HELP_TEXT.trim() + '\n');
-  process.exit(exitCode);
+  cloneArea.write(`Cloning the puzzle skeleton into ${PUZZLE_DIR}... done!`);
+  cloneArea.close();
 }
