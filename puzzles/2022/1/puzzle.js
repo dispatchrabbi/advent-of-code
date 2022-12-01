e20c6fe3e2af151730e1ee7baab460f22ffe1f4d
Eli Morris-Heft <eli.morris.heft@gmail.com>
2022-12-01 07:50:44 -0500

    2022 Day 1

--- puzzles/2022/1/puzzle.js
+++ puzzles/2022/1/puzzle.js
@@ -2,22 +2,28 @@ import chalk from 'chalk';
 import loglevel from 'loglevel';
 import { frame } from '#lib/puzzle-renderer';
 
+import { sum } from '#utils/maths';
+
 const log = loglevel.getLogger('puzzle');
 
 async function* part1(input, options = {}) {
-  const parsed = parseInput(input);
+  const elfs = parseInput(input);
 
-  return parsed;
+  const mostCalories = Math.max(...elfs.map(sum));
+
+  return mostCalories;
 }
 
 async function* part2(input, options = {}) {
   const parsed = parseInput(input);
 
-  return parsed;
+  const elfs = parsed.map(sum).sort((a, b) => b - a);
+
+  return sum(elfs.slice(0, 3));
 }
 
 function parseInput(input) {
-  return input.trim();
+  return input.trim().split('\n\n').map(elf => elf.split('\n').map(x => +x));
 }
 
 export default { part1, part2 };
