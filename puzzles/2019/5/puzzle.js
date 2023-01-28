009020156037968f3eb5b00e6b0ac3f686c159ab
Eli Morris-Heft <eli.morris.heft@gmail.com>
2023-01-28 16:37:54 -0500

    2019 Day 5

---
+++ puzzles/2019/5/puzzle.js
@@ -0,0 +1,34 @@
+import chalk from 'chalk';
+import loglevel from 'loglevel';
+import { frame } from '#lib/puzzle-renderer';
+import { Intcode } from '../common/intcode.js';
+
+const log = loglevel.getLogger('puzzle');
+
+async function* part1(input, options = { input: [ 1 ] }) {
+  const program = parseInput(input);
+
+  const computer = new Intcode(program, options.input);
+  await computer.run();
+
+  const output = computer.outputQueue;
+
+  return output[output.length - 1];
+}
+
+async function* part2(input, options = { input: [ 5 ] }) {
+  const program = parseInput(input);
+
+  const computer = new Intcode(program, options.input);
+  await computer.run();
+
+  const output = computer.outputQueue;
+
+  return output[output.length - 1];
+}
+
+function parseInput(input) {
+  return input.trimEnd().split(',').map(x => +x);
+}
+
+export default { part1, part2 };
--- puzzles/2019/common/intcode.js
+++ puzzles/2019/common/intcode.js
@@ -4,16 +4,21 @@ import loglevel from 'loglevel';
 const log = loglevel.getLogger('puzzle');
 
 class Intcode {
-  constructor(program) {
+  constructor(program, inputQueue = []) {
     this.memory = program.slice();
 
+    this.inputQueue = inputQueue.slice();
+    this.outputQueue = [];
+
     this.counter = 0;
+    this.parameterModes = [];
     this.resetFlags();
   }
 
   resetFlags() {
     this.flags = {
       halt: false,
+      jump: false,
     };
   }
 
@@ -21,10 +26,17 @@ class Intcode {
     this.resetFlags();
 
     const opcode = this.pos(this.counter);
-    const instruction = Intcode.OPCODES[opcode];
+
+    const instructionCode = opcode % 100;
+    const instruction = Intcode.OPCODES[instructionCode];
     if(!instruction) {
       throw new Error(`Unknown opcode ${opcode} at ${this.counter}`);
     }
+
+    // modes are encoded in the opcode, starting from the hundreds digit and moving *left*
+    // any modes not explicitly there are implicitly 0
+    const explicitModes = String(opcode).split('').reverse().slice(2).map(x => +x);
+    this.parameterModes = Array(instruction.args).fill(0).map((_, ix) => ix < explicitModes.length ? explicitModes[ix] : 0);
 
     const args = this.nom(instruction.args);
     await instruction.fn(args, this);
@@ -42,6 +54,20 @@ class Intcode {
     }
   }
 
+  param(val, ix) {
+    const mode = this.parameterModes[ix];
+    switch(mode) {
+      case 0:
+        // position mode: get the value at this location in memory
+        return this.pos(val);
+      case 1:
+        // immediate mode: use this value literally
+        return val;
+      default:
+        throw new Error(`Invalid parameter mode: ${mode} for parameter ${ix} (value ${val})`);
+    }
+  }
+
   pos(ix) {
     return this.memory[ix];
   }
@@ -50,20 +76,67 @@ class Intcode {
     this.memory[ix] = val;
   }
 
+  jump(ix) {
+    this.counter = ix;
+    this.flags.jump = true;
+  }
+
   nom(times = 1) {
     return this.memory.slice(this.counter + 1, this.counter + times + 1);
+  }
+
+  input() {
+    if(this.inputQueue.length > 0) {
+      return this.inputQueue.shift();
+    } else {
+      throw new Error(`No input to take`);
+    }
+  }
+
+  output(val) {
+    this.outputQueue.push(val);
   }
 
   static OPCODES = {
     1: {
       name: 'add',
       args: 3,
-      fn: ([x, y, d], cpu) => { cpu.put(d, cpu.pos(x) + cpu.pos(y)); }
+      fn: ([x, y, d], cpu) => { cpu.put(d, cpu.param(x, 0) + cpu.param(y, 1)); }
     },
     2: {
       name: 'mul',
       args: 3,
-      fn: ([x, y, d], cpu) => { cpu.put(d, cpu.pos(x) * cpu.pos(y)); }
+      fn: ([x, y, d], cpu) => { cpu.put(d, cpu.param(x, 0) * cpu.param(y, 1)); }
+    },
+    3: {
+      name: 'inp',
+      args: 1,
+      fn: ([d], cpu) => { cpu.put(d, cpu.input()); }
+    },
+    4: {
+      name: 'out',
+      args: 1,
+      fn: ([s], cpu) => { cpu.output(cpu.param(s, 0)); }
+    },
+    5: {
+      name: 'jnz', // a.k.a. jump-if-true
+      args: 2,
+      fn: ([t, c], cpu) => { if(cpu.param(t, 0) !== 0) { cpu.jump(cpu.param(c, 1)); } },
+    },
+    6: {
+      name: 'jez',
+      args: 2,
+      fn: ([t, c], cpu) => { if(cpu.param(t, 0) === 0) { cpu.jump(cpu.param(c, 1)); } },
+    },
+    7: {
+      name: 'tlt',
+      args: 3,
+      fn: ([a, b, d], cpu) => { cpu.put(d, cpu.param(a, 0) < cpu.param(b, 1) ? 1 : 0); }
+    },
+    8: {
+      name: 'teq',
+      args: 3,
+      fn: ([a, b, d], cpu) => { cpu.put(d, cpu.param(a, 0) === cpu.param(b, 1) ? 1 : 0); }
     },
     99: {
       name: 'halt',
